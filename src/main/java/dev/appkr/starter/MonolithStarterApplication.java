9aff6f1f4b49c0c9b1786e6091da6f6116aacabc
appkr <juwonkim@me.com>
2021-07-22 20:32:33 +0900

    created a new project

---
+++ src/main/java/dev/appkr/starter/MonolithStarterApplication.java
@@ -0,0 +1,13 @@
+package dev.appkr.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/appkr/starter/MonolithStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.appkr.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
